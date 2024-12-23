ab73c6a39b5cbcc41d47deb06f0042620fa46727
Edo-Andres <edo.andres.ec@gmail.com>
2024-12-23 02:17:58 -0300

    feat: implement prediction API and model loading functionality

--- challenge/model.py
+++ challenge/model.py
@@ -1,58 +1,70 @@
 import pandas as pd
-
+from sklearn.linear_model import LogisticRegression
 from typing import Tuple, Union, List
+import joblib
 
 class DelayModel:
 
-    def __init__(
-        self
-    ):
-        self._model = None # Model should be saved in this attribute.
+    def __init__(self):
+        # I
+        self._model = None  
 
     def preprocess(
         self,
         data: pd.DataFrame,
         target_column: str = None
-    ) -> Union(Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame):
+    ) -> Union[Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame]:
         """
-        Prepare raw data for training or predict.
+        Prepares the data for training or prediction.
 
         Args:
-            data (pd.DataFrame): raw data.
-            target_column (str, optional): if set, the target is returned.
+            data (pd.DataFrame): Raw data.
+            target_column (str, optional): If specified, returns the target.
 
         Returns:
-            Tuple[pd.DataFrame, pd.DataFrame]: features and target.
+            Tuple[pd.DataFrame, pd.DataFrame]: Features and target.
             or
-            pd.DataFrame: features.
+            pd.DataFrame: Only the features.
         """
-        return
+        # Create dummy variables
+        features = pd.concat([
+            pd.get_dummies(data['OPERA'], prefix='OPERA'),
+            pd.get_dummies(data['TIPOVUELO'], prefix='TIPOVUELO'),
+            pd.get_dummies(data['MES'], prefix='MES')
+        ], axis=1)
+        
+        
+        if hasattr(self._model, "feature_names_in_"):
+            # Fill missing columns with zeros
+            features = features.reindex(columns=self._model.feature_names_in_, fill_value=0)
 
-    def fit(
-        self,
-        features: pd.DataFrame,
-        target: pd.DataFrame
-    ) -> None:
+        if target_column:
+            target = data[target_column]
+            return features, target
+
+        return features
+
+    def load(self, file_path: str) -> None:
         """
-        Fit model with preprocessed data.
+        Loads a trained model from a file.
 
         Args:
-            features (pd.DataFrame): preprocessed data.
-            target (pd.DataFrame): target.
+            file_path (str): Path to the file from which the model will be loaded.
         """
-        return
+        self._model = joblib.load(file_path)
+        print(f"Model loaded from {file_path}")
 
-    def predict(
-        self,
-        features: pd.DataFrame
-    ) -> List[int]:
+    def predict(self, features: pd.DataFrame) -> List[int]:
         """
-        Predict delays for new flights.
+        Predicts delays for new flights.
 
         Args:
-            features (pd.DataFrame): preprocessed data.
+            features (pd.DataFrame): Preprocessed data.
         
         Returns:
-            (List[int]): predicted targets.
+            List[int]: Predictions.
         """
-        return
+        if not self._model:
+            raise ValueError("The model is not loaded.")
+        
+        return self._model.predict(features).tolist()
